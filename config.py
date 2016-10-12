72a606038b9dbd68bc553685240fc05f512f9a14
michal.gorecki <michal.gorecki@esnc.pl>
2016-10-12 13:35:09 +0200

    Logowanie z walidacja

---
+++ config.py
@@ -0,0 +1,7 @@
+WTF_CSRF_ENABLED = True
+SECRET_KEY = 'developer-key'
+OPENID_PROVIDERS = [
+    {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
+    {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
+    {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
+    {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
