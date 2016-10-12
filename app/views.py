eb918597bd8a183bd881479272d87c67ff4cf02c
michal.gorecki <michal.gorecki@esnc.pl>
2016-10-12 12:25:15 +0200

    Poczatek templateow

--- app/views.py
+++ app/views.py
@@ -1,6 +1,14 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello World!"
+    user = {'nickname': 'Maik'}
+
+    posts = [
+    {'author': {'nickname': 'Jon'}, 'body': "No piekny dzionek"},
+    {'author': {'nickname': 'Ela'}, 'body': "No malo piekny dzionek"}
+    ]
+
+    return render_template('index.html', title='Home', user=user, posts=posts)
